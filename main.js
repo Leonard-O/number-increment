4a45fce7b6b8cb3cefb90dacedb0018e7ae1f92c
Leonard <realcodedevs@gmail.com>
2022-02-03 17:15:23 +0300

    test commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// const body = document.querySelectorAll('body');
+const btn = document.getElementsByClassName('btn')[0];
+
+btn.addEventListener('Click', function() {
+    console.log('hello world');
+    // document.body.style.backgroundColor = 'red';
+});
