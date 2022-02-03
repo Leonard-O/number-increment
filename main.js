e4f967e0be6bf798c83aba705dd575296c505c3a
Leonard <realcodedevs@gmail.com>
2022-02-03 19:45:00 +0300

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+// const btn = document.getElementById('btn');
 // const body = document.querySelectorAll('body');
-const btn = document.getElementsByClassName('btn')[0];
+// const demo = document.getElementById('demo');
 
-btn.addEventListener('Click', function() {
-    console.log('hello world');
-    // document.body.style.backgroundColor = 'red';
+// btn.addEventListener('click', function() {
+//     demo.innerHTML = 'Hello world';
+//     document.body.style.backgroundColor = 'red';
+// });
+
+const body = document.body;
+
+// // document.body.style.backgroundColor = 'red';
+
+// const div = document.createElement('div');
+// body.appendChild(div);
+// div.innerText = 'innerText';
+
+// const p = document.createElement('p');
+// div.appendChild(p);
+// p.innerHTML = 'Paragraph';
+
+const div = document.querySelector('div');
+const spanHi = document.querySelector('#hi');
+const spanBye = document.querySelector('#bye');
+
+// spanBye.remove();
+div.append(spanBye);
+// div.removeChild(spanHi);
+
+div.addEventListener('mouseover', function() {
+    spanHi.remove();
+});
+
+div.addEventListener('mouseout', function() {
+    div.appendChild(spanHi);
 });
